00f6db26ce66f69f22c24d910aefad166e8e56b5
Dax the Deployer <dax@duckduckgo.com>
2026-04-05 19:30:20 -0400

    Add autoconsent rules for https://www.tencentcloud.com/

--- tests/generated/auto_AU_tencentcloud.com_94g.spec.ts
+++ tests/generated/auto_AU_tencentcloud.com_94g.spec.ts
@@ -1,5 +1,5 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_AU_tencentcloud.com_94g', ['https://www.tencentcloud.com/document/product/215/35529'], {
+generateCMPTests('auto_AU_tencentcloud.com_94g', ['https://www.tencentcloud.com/'], {
     testOptIn: false,
     testSelfTest: true,
     onlyRegions: ['AU'],
